ac0f275dc6eea2b23d2740cb54d1f5bf7ef654bf
PrMax <89717747+PrMax@users.noreply.github.com>
2023-09-18 20:12:04 +0500

    Initial Commit

---
+++ Ggg/GggApp.swift
@@ -0,0 +1,17 @@
+//
+//  GggApp.swift
+//  Ggg
+//
+//  Created by Batman 👀 on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
